c38f35d80a9c15a8ba0b8532fbb4d2b2a055a4a8
nisha tamang <tamang.nisha2063@gmail.com>
2026-03-01 09:08:18 +0545

    c is added to my day2 code

---
+++ day2/sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, sum;
+
+    cout << "Enter three numbers: ";
+    cin >> a >> b >> c;
+
+    sum = a + b + c ;
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
